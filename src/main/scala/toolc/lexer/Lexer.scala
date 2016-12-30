3701fdd0c80125d9253664cb16b81384e2aac71c
MikaelMorales <mikael.moralesgonzalez@epfl.ch>
2016-12-30 11:29:38 +0100

    update syntax value class

--- src/main/scala/toolc/lexer/Lexer.scala
+++ src/main/scala/toolc/lexer/Lexer.scala
@@ -169,7 +169,7 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
         case '*' => consume(); TIMES()
         case '/' => consume(); DIV()
 
-        case l if(l.isLetter) => {
+        case l if l.isLetter || l == '@' => {
           val identifier = new StringBuilder()
           identifier.append(currentChar)
           consume()
